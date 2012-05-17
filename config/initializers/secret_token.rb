ca4a6646d0344edaf18fe84c123754cbf7b102c6
Pascal Fischer <s052647@htw-berlin.de>
2012-05-17 13:13:50 +0200

    massig

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BodykitMe::Application.config.secret_token = 'b74b94472d772b36a4e36c2179591b28ad4a0de6b1c5882c0ffeba30a577d571b249addd6d37b294af9fe67c68024c7bac9066148b1a3cdba7283a5c447d7c49'
