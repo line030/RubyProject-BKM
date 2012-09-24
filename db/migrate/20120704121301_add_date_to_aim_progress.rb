e10da1178cb9840f73a5dadda0ccbdea5a777e27
Pascal Fischer <s0526471@htw-berlin.de>
2012-09-25 00:01:28 +0200

    deployment bugfix

--- db/migrate/20120704121301_add_date_to_aim_progress.rb
+++ db/migrate/20120704121301_add_date_to_aim_progress.rb
@@ -1,6 +1,6 @@
 class AddDateToAimProgress < ActiveRecord::Migration
   def change
-      add_column :aim_progresses, :date, :date_time
+      add_column :aim_progresses, :date, :datetime
   end
 end
 
