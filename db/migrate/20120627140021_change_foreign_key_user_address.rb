27acb3ab62556cd43601872ba0b900014b8fa02e
Pascal Fischer <s0526471@htw-berlin.de>
2012-09-24 23:58:32 +0200

    deployment bugfix

--- db/migrate/20120627140021_change_foreign_key_user_address.rb
+++ db/migrate/20120627140021_change_foreign_key_user_address.rb
@@ -1,6 +1,6 @@
 class ChangeForeignKeyUserAddress < ActiveRecord::Migration
   def change
-    remove_column :users, :address_id, :integer
+    remove_column :users, :address_id
     add_column :addresses, :user_id, :integer
   end
 end
