4784fb64b82783e7b0f7ef21f48d030a12a52596
Pascal Fischer <s0526471@htw-berlin.de>
2012-10-24 00:40:30 +0200

    changed route scope to /bodykitme to be able to deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 BodykitMe::Application.routes.draw do
-
+	scope "/bodykitme" do
   resources :aim_progresses
 
   #get "password_resets/new"
@@ -157,4 +157,5 @@ BodykitMe::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  end 
 end
