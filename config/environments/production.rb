a916896d3252abc503adb3033821df014cde0723
Pascal Fischer <s0526471@htw-berlin.de>
2012-10-29 20:02:08 +0100

    deployment env update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 BodykitMe::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+	config.action_controller.relative_url_root = "/bodykitme"
+	
   # Code is not reloaded between requests
   config.cache_classes = true
 
