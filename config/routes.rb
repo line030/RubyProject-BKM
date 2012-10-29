eab098ae4480f01b728dc3e48cac1702588cb109
Pascal Fischer <s0526471@htw-berlin.de>
2012-10-29 23:27:42 +0100

    Deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 BodykitMe::Application.routes.draw do
-  scope "bodykitme" do
     resources :aim_progresses
 
     #get "password_resets/new"
@@ -157,5 +156,3 @@ BodykitMe::Application.routes.draw do
     # Note: This route will make all actions in every controller accessible via GET requests.
     # match ':controller(/:action(/:id))(.:format)'
   end
-
-end
