b8fd0b4d54ff066cb0f5a7ceb14cf2f6e1c81d35
Pascal Fischer <s0526471@htw-berlin.de>
2012-11-14 23:15:24 +0100

    fuck the fucking fuck capistrano

--- config/setup_load_paths.rb
+++ config/setup_load_paths.rb
@@ -1,14 +1,15 @@
 if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
   begin
-    gems_path = ENV['MY_RUBY_HOME'].split(/@/)[0].sub(/rubies/,'gems')
-    ENV['GEM_PATH'] = "#{gems_path}:#{gems_path}@global"
+    rvm_path     = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
+    rvm_lib_path = File.join(rvm_path, 'lib')
+    $LOAD_PATH.unshift rvm_lib_path
     require 'rvm'
     RVM.use_from_path! File.dirname(File.dirname(__FILE__))
   rescue LoadError
-    raise "RVM gem is currently unavailable."
+    # RVM is unavailable at this point.
+    raise "RVM ruby lib is currently unavailable."
   end
 end
-
-# If you're not using Bundler at all, remove lines bellow
+ 
 ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', File.dirname(__FILE__))
 require 'bundler/setup'
