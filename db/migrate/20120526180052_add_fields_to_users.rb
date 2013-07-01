515a46b67d1ba124ff6d4dadfea84cde5840011e
Pascal Fischer <s0526471@htw-berlin.de>
2013-07-01 23:03:50 +0200

    change migration

--- db/migrate/20120526180052_add_fields_to_users.rb
+++ db/migrate/20120526180052_add_fields_to_users.rb
@@ -4,7 +4,7 @@ class AddFieldsToUsers < ActiveRecord::Migration
     add_column :users, :forename, :string
     add_column :users, :date_of_birth, :date
     add_column :users, :gender, :string
-    add_column :users, :enabled, :boolean, :default => 1
+    add_column :users, :enabled, :boolean, :default => true
     add_column :users, :address_id, :integer
   end
 end
