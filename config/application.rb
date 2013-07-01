757c4cfd5e7da24a7efd1407d6654da4b2e7c8bf
Pascal Fischer <s0526471@htw-berlin.de>
2013-07-01 23:17:02 +0200

    jquery-uirails added

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,18 @@ module BodykitMe
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.cache_classes = true
+
+    config.consider_all_requests_local       = false
+    config.action_controller.perform_caching = true
+
+    config.serve_static_assets = true
+
+    config.assets.compress = true
+
+    config.assets.compile = true
+
+    config.assets.digest = true
   end
 end
